038d9c4389e8fa2aa61886ebcda547b59ddf9392
jvrboy <justinsimpsad@gmail.com>
2026-08-06 10:07:33 +0200

    feat: add MainApplication.cs for MAUI platform build

---
+++ src/TraderApp/Platforms/Android/MainApplication.cs
@@ -0,0 +1,12 @@
+using Android.App;
+
+namespace TraderUI;
+
+[Application]
+public class MainApplication : MauiApplication
+{
+    public MainApplication(IntPtr handle, JniHandleOwnership ownership)
+        : base(handle, ownership) { }
+
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
