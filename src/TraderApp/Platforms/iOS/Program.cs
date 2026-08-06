ed1af7aaa472a1deaf5ff1e35ef6d8575007a13b
jvrboy <justinsimpsad@gmail.com>
2026-08-06 10:07:36 +0200

    feat: add Program.cs for MAUI platform build

---
+++ src/TraderApp/Platforms/iOS/Program.cs
@@ -0,0 +1,12 @@
+using ObjCRuntime;
+using UIKit;
+
+namespace TraderUI;
+
+public static class Program
+{
+    static void Main(string[] args)
+    {
+        UIApplication.Main(args, null, typeof(AppDelegate));
+    }
+}
